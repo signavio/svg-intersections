c1d2a7c9039f894ca8244f7abdadab3dd2349385
Valentin <valentin.ranshakov@sap.com>
2024-01-02 15:57:42 +0100

    add sonar arguments

--- analyse.js
+++ analyse.js
@@ -1,14 +1,34 @@
 const scanner = require("sonarqube-scanner");
 
+let options = {
+    "sonar.sources": "lib",
+    "sonar.tests": "test",
+    "sonar.typescript.lcov.reportPaths": "coverage/lcov.info",
+    "sonar.projectKey": "com.sap.signavio.svg-intersections"
+}
+
+process.argv.forEach(function (val, index, array) {
+if (index > 1) {
+    const val_arr = val.split("=")
+    if (val_arr[0] === "sonar.branch.name"){
+        options.push({"sonar.branch.name":val_arr[1]})
+    }
+    if (val_arr[0] === "sonar.pullrequest.base"){
+        options.push({"sonar.pullrequest.base":val_arr[1]})
+    }
+    if (val_arr[0] === "sonar.pullrequest.key"){
+        options.push({"sonar.pullrequest.key":val_arr[1]})
+    }
+    if (val_arr[0] === "sonar.pullrequest.branch"){
+        options.push({"sonar.pullrequest.branch":val_arr[1]})
+    }
+}
+});
+
 scanner(
   {
     serverUrl: "https://sonar.tools.sap/",
-    options: {
-      "sonar.sources": "lib",
-      "sonar.tests": "test",
-      "sonar.typescript.lcov.reportPaths": "coverage/lcov.info",
-      "sonar.projectKey": "com.sap.signavio.svg-intersections"
-    },
+    options: options,
   },
   () => {
     // callback is required
