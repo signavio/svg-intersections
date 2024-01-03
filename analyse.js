8b05e8d8cf6b0f465e51d42c2bcdaad432cf7c98
Valentin <142783629+valentin-sap@users.noreply.github.com>
2024-01-03 09:40:36 +0100

    Update analyse.js
    Co-authored-by: David Bialik <david.bialik@gmail.com>

--- analyse.js
+++ analyse.js
@@ -7,22 +7,9 @@ let options = {
     "sonar.projectKey": "com.sap.signavio.svg-intersections"
 }
 
-process.argv.forEach(function (val, index, array) {
-if (index > 1) {
-    const val_arr = val.split("=")
-    if (val_arr[0] === "sonar.branch.name"){
-        options["sonar.branch.name"] = val_arr[1]
-    }
-    if (val_arr[0] === "sonar.pullrequest.base"){
-        options["sonar.pullrequest.base"] = val_arr[1]
-    }
-    if (val_arr[0] === "sonar.pullrequest.key"){
-        options["sonar.pullrequest.key"]=val_arr[1]
-    }
-    if (val_arr[0] === "sonar.pullrequest.branch"){
-        options["sonar.pullrequest.branch"]=val_arr[1]
-    }
-}
+process.argv.slice(2).forEach(function (argument) {
+  const [key, value] = argument.split("=");
+  options[key] = value;
 });
 
 scanner(
